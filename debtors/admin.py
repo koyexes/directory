390aeba86ffddbcf52f2a0f016d3e42924863633
koyexes <koyagabriel@gmail.com>
2016-09-29 16:44:51 +0100

    Administration template

--- debtors/admin.py
+++ debtors/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Paylater_Debtors
 
 # Register your models here.
+
+class PaylaterDebtors(admin.ModelAdmin):
+    list_display = ('AccountHolderName')
+
+
+admin.site.register(Paylater_Debtors, PaylaterDebtors)
--- debtors/models.py
+++ debtors/models.py
@@ -3,3 +3,15 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Paylater_Debtors(models.Model):
+    AccountHolderName  = models.CharField(max_length = 255)
+    Employer = models.CharField(max_length = 255)
+    TotalDue = models.BigIntegerField()
+    Balance = models.BigIntegerField()
+    DaysInArrears = models.IntegerField()
+    MobilePhone = models.CharField(max_length = 15)
+
+
+
+
+
