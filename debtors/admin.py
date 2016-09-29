2c9382b97919a266ce5f8b1167331853eb39f4af
koyexes <koyagabriel@gmail.com>
2016-09-29 17:05:58 +0100

    Models work

--- debtors/admin.py
+++ debtors/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from .models import Paylater_Debtors
+from .models import Paylater_Debtor
 
 # Register your models here.
 
 class PaylaterDebtors(admin.ModelAdmin):
-    list_display = ('AccountHolderName')
+    list_display = ('AccountHolderName', 'Employer', 'TotalDue', 'Balance', 'DaysInArrears', 'MobilePhone')
+    search_fields = ['AccountHolderName', 'Employer', 'TotalDue', 'Balance', 'DaysInArrears', 'MobilePhone']
 
 
-admin.site.register(Paylater_Debtors, PaylaterDebtors)
+admin.site.register(Paylater_Debtor, PaylaterDebtors)
--- debtors/models.py
+++ debtors/models.py
@@ -3,13 +3,13 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
-class Paylater_Debtors(models.Model):
-    AccountHolderName  = models.CharField(max_length = 255)
-    Employer = models.CharField(max_length = 255)
+class Paylater_Debtor(models.Model):
+    AccountHolderName  = models.CharField(max_length = 255, verbose_name = 'Account Holder')
+    Employer = models.CharField(max_length = 255, blank = True)
     TotalDue = models.BigIntegerField()
     Balance = models.BigIntegerField()
     DaysInArrears = models.IntegerField()
-    MobilePhone = models.CharField(max_length = 15)
+    MobilePhone = models.CharField(max_length = 15, blank = True)
 
 
 
